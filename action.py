78fea224ebb2c7fab95c1e22694fc535e6470973
DeepGPT0504 <sungjin4451@gmail.com>
2025-06-12 23:15:50 +0900

    action update

--- action.py
+++ action.py
@@ -7,9 +7,10 @@ class player_action(): #player가 사용하는 행동
         self.money = int(money) #플레이어 보유 금액 저장
         self.deck = deck #플레이어가 가지고 있는 덱 저장
         self.say_fold = say_fold #플레이어의 폴드 선언 여부
-        self.say_call = say_call #player
-        self.say_raise = say_raise
+        self.say_call = say_call #플레이어의 콜 선언 여부
+        self.say_raise = say_raise #플레이어의 레이즈 선언 여부
 
+    #플레이어의 행동 모음
     def actions(self, bet_money):
         while True:
             if self.say_fold == True:
@@ -17,16 +18,26 @@ class player_action(): #player가 사용하는 행동
                 return bet_money
             else:
                 question = input(f'무엇을 하시겠습니까?(레이즈/콜/폴드) \n 현재 베팅 금액:{bet_money}: ')
+                
+                #레이즈
                 if question == '레이즈':
-                    question_2 = int(input(f'얼마를 레이즈하시겠습니까?(현재 판돈{bet_money},현재 잔액{self.money}): '))
-                    if question_2 > self.money:
-                        print('잔액이 부족합니다')
-                    else:
-                        self.say_raise = True
-                        return bet_money + question_2
+                    while True:
+
+                        question_2 = input(f'얼마를 레이즈하시겠습니까?(현재 판돈{bet_money},현재 잔액{self.money})(취소): ')
+                        if type(question_2) == int and question_2 > self.money:
+                            print('잔액이 부족합니다')
+                        elif question_2 == '취소':
+                            self.actions(bet_money)
+                        else: 
+                            self.say_raise = True
+                            return bet_money + int(question_2)
+                    
+                #콜
                 elif question == '콜':
                     self.say_call = True
                     return 0
+                
+                #폴드
                 elif question == '폴드':
                     self.say_fold = True
                     return 0
@@ -57,43 +68,44 @@ class computer_action(): #computer_player가 사용하는 행동
                 return betting_money
             
         else: #정석 베팅
-            if z_score == 100: 
+            if z_score == 100: #현재 보유 금액의 50%부터 80% 사이로 랜덤하게 베팅
                 betting_money = self.money * (rd.randint(50,80)/100)
                 return int(betting_money)
             
-            elif z_score == 90:
+            elif z_score == 90: #현재 보유 금액의 40%부터 60% 사이로 랜덤하게 베팅
                 betting_money = self.money * (rd.randint(40,60)/100)
                 return int(betting_money)
             
-            elif z_score == 80:
+            elif z_score == 80: #현재 보유 금액의 30%부터 50% 사이로 랜덤하게 베팅
                 betting_money = self.money * (rd.randint(30,50)/100)
                 return int(betting_money)
             
-            elif z_score == 70:
-                betting_money = self.money * (rd.randint(10,40)/100)
+            elif z_score == 70: #현재 보유 금액의 15%부터 50% 사이로 랜덤하게 베팅
+                betting_money = self.money * (rd.randint(15,50)/100)
                 return int(betting_money)
             
-            elif z_score == 60:
+            elif z_score == 60: #현재 보유 금액의 5%부터 45% 사이로 랜덤하게 베팅
                 betting_money = self.money * (rd.randint(5,45)/100)
                 return int(betting_money)
             
-            elif z_score == 50:
+            elif z_score == 50: #현재 보유 금액의 5%부터 30% 사이로 랜덤하게 베팅
                 betting_money = self.money * (rd.randint(5,30)/100)
                 return int(betting_money)
             
-            elif z_score == 40:
+            elif z_score == 40: #현재 보유 금액의 5%부터 30% 사이로 랜덤하게 베팅
                 betting_money = self.money * (rd.randint(5,30)/100)
                 return int(betting_money)
             
-            elif z_score == 30:
+            elif z_score == 30: #현재 보유 금액의 5%부터 30% 사이로 랜덤하게 베팅
                 betting_money = self.money * (rd.randint(5,30)/100)
                 return int(betting_money)
             
-            else:
+            else: #현재 보유 금액의 5% 부터 20% 사이로 랜덤하게 베팅
                 betting_money = self.money * (rd.randint(5,20)/100)
                 return int(betting_money)
             
         
+    #컴퓨터 플레이어가 하는 행동 모음음
     def actions(self,bet_money,z_score,bank):
         degree = self.cal_betting_momney(z_score)
         print(degree)
